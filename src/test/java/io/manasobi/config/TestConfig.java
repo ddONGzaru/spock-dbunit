e6798589d5f401644c759f60608ad7daa59789f9
tw.jang <tw.jang@anypointmedia.com>
2015-09-06 11:47:02 +0900

    Initial commit

---
+++ src/test/java/io/manasobi/config/TestConfig.java
@@ -0,0 +1,12 @@
+package io.manasobi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2015-09-06.
+ */
+@Configuration
+public class TestConfig {
+
+
+}
